3e87c2fac4498415022ffef4f38bfbaed11c28ba
Marcus <Agbarakum@gmail.com>
2023-04-25 03:23:22 -0700

    Added integral functions

--- shell.h
+++ shell.h
@@ -9,40 +9,40 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-/* Removes new line */
+/* it removes new line */
 void nnline(char *in);
 
-/* Writes a char in stdout */
+/* it will Write a char in stdout */
 int ponchar(char c);
 
-/* Prints a string */
+/* it will Print a string */
 void str_printer(char *s);
 
-/* Compares 2 strings */
+/* Tries to Compares 2 strings */
 int _strcmp(char *s1, char *s2);
 
-/* Measures the lenght of a string */
+/* It will  measure the lenght of a string */
 int _strlen(char *s);
 
-/* Concatenates 2 strings */
+/* it Concatenates 2 strings */
 char *_strcat(char *dest, char *src);
 
-/* Copy a string pointed to src */
+/* it Copy's a string pointed to src */
 char *_strcpy(char *dest, char *src);
 
-/* Duplicates a string */
+/* it Duplicates a string */
 char *_strdup(char *str);
 
-/* Will parse/separate text of a string into tokens */
+/* it will parse/separate text of a string into tokens */
 char **parsero(char *l);
 
-/* Helps to implement exit env_printer builtins */
+/* it will help to implement exit env_printer builtins */
 void builtins(char *input, char **args, char **env, int *ex_st);
 
-/* Prints the enviroment variable */
+/* it prints the enviroment variable */
 int env_printer(char **env, int *ex_sts);
 
-/* Print a special number */
+/* Prints a special number */
 void int_printer(int *atte);
 
 /* Prints the path string */
@@ -51,13 +51,13 @@ char *str_path(char *dir, char *fst);
 /* Prints the enviroment PATH */
 int _path(char *fst, char **inpt, char **env, int *ex_st);
 
-/* Executes process in the main shell */
+/* it executes process in the main shell */
 void _xcute(int status, char **args, int *ex_st, int *cnt);
 
-/* Handles special characters and errors at typing commands */
+/* it handles special characters and errors at typing commands */
 int super_char(char *buffer, ssize_t bytes, int *ex_st);
 
-/* Helps to free a matrix of pointers */
+/* it helps to free a matrix of pointers */
 void free_matrix(char **matrix, int height);
 #endif
 
