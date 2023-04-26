281ebea7cc674b035a4223e4979df72b0a353a8b
Marcus <Agbarakum@gmail.com>
2023-04-25 22:12:48 -0700

    my shell files

--- _cd.c
+++ _cd.c
@@ -1,10 +1,10 @@
 #include "shell.h"
 
 /**
- * c_strcat - will concatenate two strings ignoring the first character ("~" in cd)
- * @dest: is a string to be appended to
- * @src:is the string to append
- * Return: is the concatenated string
+ * c_strcat - concatenate two strings ignoring the first character ("~" in cd)
+ * @dest: string to be appended to
+ * @src: string to append
+ * Return: concatenated string
  */
 char *c_strcat(char *dest, char *src)
 {
@@ -12,7 +12,6 @@ char *c_strcat(char *dest, char *src)
 	int len2 = 0;
 	int total_len = 0;
 	int j = 0;
-
 
 	while (dest[len] != '\0')
 	{
@@ -25,10 +24,9 @@ char *c_strcat(char *dest, char *src)
 		total_len++;
 	}
 
-	
 	dest = _realloc(dest, len, sizeof(char) * total_len + 1);
 
-	j = 1; 
+	j = 1;
 	while (src[j] != '\0')
 	{
 		dest[len] = src[j];
@@ -41,11 +39,11 @@ char *c_strcat(char *dest, char *src)
 }
 
 /**
- * c_setenv - custom _setenv will concatenate the string first before setting
- * @env: is the environmental variable linked list
- * @name:is the  environmental variable name
- * @dir:is the  directory path 
- * Return: (0) on success 
+ * c_setenv - custom _setenv by concatenating string first before setting
+ * @env: environmental variable linked list
+ * @name: environmental variable name (e.g. "OLDPWD")
+ * @dir: directory path (e.g. "/home/vagrant/directory1")
+ * Return: 0 on success (e.g. "OLDPWD=/home/vagrant/directory1")
  */
 int c_setenv(list_t **env, char *name, char *dir)
 {
@@ -56,9 +54,8 @@ int c_setenv(list_t **env, char *name, char *dir)
 	cat = _strdup(name);
 	cat = _strcat(cat, "=");
 	cat = _strcat(cat, dir);
-	index = find_env(*env, name); 
+	index = find_env(*env, name);
 
-	
 	holder = *env;
 	while (j < index)
 	{
@@ -72,9 +69,9 @@ int c_setenv(list_t **env, char *name, char *dir)
 }
 
 /**
- * cd_only -will  change directory to home
- * @env: will bring in env linked list to update PATH and OLDPWD
- * @current: will bring in current working directotry
+ * cd_only - change directory to home
+ * @env: bring in environmental linked list to update PATH and OLDPWD
+ * @current: bring in current working directotry
  */
 void cd_only(list_t *env, char *current)
 {
@@ -92,13 +89,13 @@ void cd_only(list_t *env, char *current)
 	free(home);
 }
 /**
- * cd_execute -will  execute the cd
- * @env: brings environmental linked list to update PATH and OLDPWD
- * @current: brings in current working directotry
- * @dir: brings in dir path to change to
- * @str: brings in the 1st argument to write out the error
- * @num: brings in the line number to write out the error
- * Return: (0) if success (2) for fail
+ * cd_execute - executes the cd
+ * @env: bring in environmental linked list to update PATH and OLDPWD
+ * @current: bring in current working directotry
+ * @dir: bring in directory path to change to
+ * @str: bring in the 1st argument to write out error
+ * @num: bring in the line number to write out error
+ * Return: 0 if success 2 if fail
  */
 int cd_execute(list_t *env, char *current, char *dir, char *str, int num)
 {
@@ -124,26 +121,26 @@ int cd_execute(list_t *env, char *current, char *dir, char *str, int num)
 }
 
 /**
- * _cd - used to change directory
- * @str: is the user's typed in command
- * @env: is an enviromental linked list to retrieve HOME and OLDPWD paths
- * @num: is the nth user's command, to be used at error message
- * Return: (0) if success (<S-F9>2) for fail
+ * _cd - change directory
+ * @str: user's typed in command
+ * @env: enviromental linked list to retrieve HOME and OLDPWD paths
+ * @num: nth user command, to be used at error message
+ * Return: 0 if success 2 if failed
  */
 int _cd(char **str, list_t *env, int num)
 {
 	char *current = NULL, *dir = NULL;
 	int exit_stat = 0;
 
-	current = getcwd(current, 0); 
+	current = getcwd(current, 0);
 	if (str[1] != NULL)
 	{
-		if (str[1][0] == '~') 
+		if (str[1][0] == '~')
 		{
 			dir = get_env("HOME", env);
 			dir = c_strcat(dir, str[1]);
 		}
-		else if (str[1][0] == '-');
+		else if (str[1][0] == '-')
 		{
 			if (str[1][1] == '\0')
 				dir = get_env("OLDPWD", env);
@@ -164,6 +161,6 @@ int _cd(char **str, list_t *env, int num)
 	}
 	else
 		cd_only(env, current);
-	free_double_ptr(str); 
+	free_double_ptr(str);
 	return (exit_stat);
 }
